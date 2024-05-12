9bf88c509e7d2021110eb493b1e3939341ddb02b
Zimmmermannn <omrawte10@gmail.com>
2024-05-12 18:31:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let Displayel = document.getElementById("display")
+let displayEl = document.getElementById("display02")
+let Count=0
+let count=0
+function addone(){
+    Additone = count += 1
+    Displayel.textContent = Additone
+}
+
+function addtwo(){
+    Addittwo = count += 2
+    Displayel.textContent = Addittwo
+}
+
+function addthree(){
+    Additthree = count += 3
+    Displayel.textContent = Additthree
+}
+
+function addoneaway(){
+    Additone = Count += 1
+    displayEl.textContent = Additone
+}
+
+function addtwoaway(){
+    Addittwo = Count += 2
+    displayEl.textContent = Addittwo
+}
+
+function addthreeaway(){
+    Additthree = Count += 3
+    displayEl.textContent = Additthree
+}
